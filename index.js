510fd726913c146eb8c4438a9b8a8283f93bbd7e
unknown <mikayonekawa@gmail.com>
2025-07-22 13:50:47 -0300

    Usar variáveis de ambiente em vez de creds.json

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const cors = require('cors');
 const app = express();
 
 // Carrega as credenciais
-const keys = require('./creds.json');
+const keys = {
+  type: 'service_account',
+  project_id: 'SEU_PROJECT_ID',
+  private_key_id: 'SEU_PRIVATE_KEY_ID',
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: 'SEU_CLIENT_ID',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: 'URL_DO_CERTIFICADO'
+};
+
 
 // Configura o Google Sheets API
 const auth = new google.auth.GoogleAuth({
