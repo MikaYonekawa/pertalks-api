47c1b19bd879f293cda1b9b12189aa6b710a41fa
unknown <mikayonekawa@gmail.com>
2025-07-22 14:02:42 -0300

    corrigindo

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 // Carrega as credenciais
 const keys = {
   type: 'service_account',
-  project_id: 'SEU_PROJECT_ID',
-  private_key_id: 'SEU_PRIVATE_KEY_ID',
+  project_id: 'pertalks-api',
+  private_key_id: '9a82d06b23478d187f34520efd51b4be39896e51',
   private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  client_id: 'SEU_CLIENT_ID',
+  client_id: '104699692822257204188',
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: 'URL_DO_CERTIFICADO'
+  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/pertalks-api-service%40pertalks-api.iam.gserviceaccount.com'
 };
 
 
