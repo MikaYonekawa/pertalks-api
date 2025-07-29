76467b4be1ba697ebe209f294fd25f371513287c
unknown <mikayonekawa@gmail.com>
2025-07-28 22:26:16 -0300

    atualizando a mensagem

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def enviar_email():
     titulo = dados.get('palestra')
     senha_palestra = dados.get('senha')
 
-    msg = MIMEText(f"Olá {nome or 'participante'},\n\nVocê está inscrito na palestra '{titulo}'.\n\nSenha de acesso: {senha_palestra}\n\nAté lá!")
+    msg = MIMEText(f"Olá {nome or 'participante'},\nObrigado por se inscrever na plestra '{titulo}'.\nAqui está a sua senha de acesso: {senha_palestra}\nGuarde essa senha para acessar o evento. Você a usará no aplicativo para fazer o check-in no local.\n\nAinda não baixou o app Pertalks? Clique aqui: www.pertalks.com.br\n\nAtenciosamente,\nEquipe do evento")
     msg['Subject'] = f"Confirmação de Inscrição - {titulo}"
     msg['From'] = EMAIL
     msg['To'] = email
@@ -36,3 +36,5 @@ import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
